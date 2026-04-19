389217ccd27a904b0546896914224051bd6a0588
Gio-Midonz-Bots <gmidonz09@gmail.com>
2026-04-19 14:30:36 -0300

    Classe bootcamp

---
+++ Desafio-Abstraindo-POO/src/src/main/java/model/Bootcamp.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    protected String nome;
+    protected String descricao;
+    protected LocalDate dataInicial = LocalDate.now();
+    protected final LocalDate dataFinal = dataInicial.plusDays(45);
+    protected Set<Estudante> alunosInscritos = new HashSet<>();
+    protected Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(LocalDate dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Estudante> getAlunosInscritos() {
+        return alunosInscritos;
+    }
+
+    public void setAlunosInscritos(Set<Estudante> alunosInscritos) {
+        this.alunosInscritos = alunosInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(alunosInscritos, bootcamp.alunosInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, alunosInscritos, conteudos);
+    }
+}
